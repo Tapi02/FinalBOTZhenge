3c35fc679eb218ca6e77cd22c479d598c04c566b
Tapi02 <sheratapi@gmail.com>
2025-07-15 06:35:50 +0500

    main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import (
-    Application,
+    ApplicationBuilder,
     CommandHandler,
     ContextTypes,
 )
@@ -15,12 +15,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает ✅")
 
 async def main():
-    application = Application.builder().token(TOKEN).build()
-    
-    application.add_handler(CommandHandler("start", start))
-    
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
     print("✅ Бот запущен")
-    await application.run_polling()
+    await app.run_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
